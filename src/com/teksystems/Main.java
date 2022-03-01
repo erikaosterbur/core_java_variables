6fca9a6964599026903c843be75dee471224a5ce
erikaosterbur <erikaosterbur@gmail.com>
2022-03-01 14:16:32 -0600

    Initial commit and problem one finished

---
+++ src/com/teksystems/Main.java
@@ -0,0 +1,19 @@
+package com.teksystems;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    JavaBasicsClass();
+    }
+
+    public static void JavaBasicsClass () {
+        //Write a program that declares 2 integer variables, assigns an integer to each, and adds them together.
+        // Assign the sum to a variable.
+        // Print out the result.
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+}
